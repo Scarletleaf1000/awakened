a532181867e56384b89ce7b34a8cc437c671eade
ScarletLeaf1000 <ScarletLeaf1000@gmail.com>
2026-07-23 16:20:26 -0700

    Tweaked how the breath overlay displays in guis

--- src/main/java/me/scarletleaf1000/awakened/client/BreathHudOverlay.java
+++ src/main/java/me/scarletleaf1000/awakened/client/BreathHudOverlay.java
@@ -5,10 +5,12 @@ import me.scarletleaf1000.awakened.breath.BreathProvider;
 import me.scarletleaf1000.awakened.client.screens.AbstractAwakeningScreen;
 import me.scarletleaf1000.awakened.heightening.Heightening;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.event.RenderGuiEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -19,12 +21,27 @@ public class BreathHudOverlay {
     private static int lastChangeTick;
 
     @SubscribeEvent
-    public static void onRenderGui(RenderGuiOverlayEvent.Post event) {
+    public static void onRenderGui(RenderGuiEvent.Post event) {
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.player == null || mc.options.hideGui || mc.screen != null) {
+            return;
+        }
+        renderBreath(mc, event.getGuiGraphics());
+    }
+
+    @SubscribeEvent
+    public static void onScreenRenderPost(ScreenEvent.Render.Post event) {
         Minecraft mc = Minecraft.getInstance();
         if (mc.player == null || mc.options.hideGui) {
             return;
         }
+        if (!(event.getScreen() instanceof AbstractContainerScreen<?>) && !(event.getScreen() instanceof AbstractAwakeningScreen)) {
+            return;
+        }
+        renderBreath(mc, event.getGuiGraphics());
+    }
 
+    private static void renderBreath(Minecraft mc, GuiGraphics guiGraphics) {
         mc.player.getCapability(BreathProvider.BREATH).ifPresent(breath -> {
             int currentBreath = breath.getBreath();
             if (currentBreath != lastBreath) {
@@ -39,7 +56,7 @@ public class BreathHudOverlay {
             Component text = Component.translatable("gui.awakened.hud.breath", currentBreath, heightening.getDisplayName());
             int x = 5;
             int y = mc.getWindow().getGuiScaledHeight() - 15;
-            event.getGuiGraphics().drawString(mc.font, text, x, y, 0x0000FF, true);
+            guiGraphics.drawString(mc.font, text, x, y, 0x0000FF, true);
         });
     }
 
