f82789b808e435b7aa6abe093732c778d90f9ffa
ScarletLeaf1000 <ScarletLeaf1000@gmail.com>
2026-07-26 14:44:38 -0700

    Fixed bug with config and added new configs

--- src/main/java/me/scarletleaf1000/awakened/Config.java
+++ src/main/java/me/scarletleaf1000/awakened/Config.java
@@ -20,8 +20,25 @@ public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     public static final ForgeConfigSpec.IntValue MAX_NIGHTBLOODS = BUILDER
-            .comment("Maximum number of Nightblood swords that can be crafted per world.")
-            .defineInRange("maxNightbloods", 1, 0, Integer.MAX_VALUE);
+            .comment("Maximum number of Nightblood swords that can be crafted per world. (-1 for infinite)")
+            .defineInRange("maxNightbloods", 1, -1, Integer.MAX_VALUE);
+
+    public static final ForgeConfigSpec.IntValue BREATH_MIN_PRICE = BUILDER
+            .comment("Minimum emerald cost per Breath when trading with a villager.")
+            .defineInRange("breathMinPrice", 16, 0, Integer.MAX_VALUE);
+
+    public static final ForgeConfigSpec.IntValue BREATH_MAX_PRICE = BUILDER
+            .comment("Maximum emerald cost per Breath when trading with a villager.",
+                    "Each Breath a villager has will roll a random cost between breathMinPrice and breathMaxPrice.")
+            .defineInRange("breathMaxPrice", 32, 0, Integer.MAX_VALUE);
+
+    public static final ForgeConfigSpec.IntValue BREATH_MIN_DISCOUNT_PRICE = BUILDER
+            .comment("The lowest emerald cost a single Breath can be reduced to by Hero of the Village discounts.")
+            .defineInRange("breathMinDiscountPrice", 2, 0, Integer.MAX_VALUE);
+
+    public static final ForgeConfigSpec.IntValue BREATH_DISCOUNT_PER_LEVEL = BUILDER
+            .comment("Emerald discount applied per Hero of the Village level when purchasing Breath.")
+            .defineInRange("breathDiscountPerLevel", 8, 0, Integer.MAX_VALUE);
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 }
